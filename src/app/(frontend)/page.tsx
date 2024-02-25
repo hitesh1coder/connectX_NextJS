eb89910df47ab0af6e61fc4d64b74d84634519c1
codewitHK1 <kumarhk1002@gmail.com>
2024-02-25 11:56:21 +0530

    infinite scroll added

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -6,15 +6,30 @@ export default async function Home() {
   const supabase = createClient(cookies());
   const { data } = await supabase.auth.getSession();
 
-  const { data: posts, error: error } = await supabase
-    .rpc("get_posts_with_likes", {
-      request_user_id: data.session?.user.id,
-    })
-    .order("post_id", { ascending: false });
+  const {
+    data: posts,
+    error: error,
+    count,
+  } = await supabase
+    .rpc(
+      "get_posts_with_likes",
+      {
+        request_user_id: data.session?.user.id,
+      },
+      {
+        count: "exact",
+      }
+    )
+    .order("post_id", { ascending: false })
+    .range(0, 2);
   return (
     <div className="my-5">
       {posts && posts.length > 0 && (
-        <Posts user={data.session?.user!} data={posts} />
+        <Posts
+          user={data.session?.user!}
+          data={posts}
+          totalPosts={count ?? 0}
+        />
       )}
     </div>
   );
--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -3,16 +3,59 @@ import React, { useState, useEffect } from "react";
 import PostCard from "./PostCard";
 import { User } from "@supabase/supabase-js";
 import { createClient } from "@/lib/supabase/supabaseClient";
+import Loading from "@/app/(frontend)/loading";
+import { useInView } from "react-intersection-observer";
+import { toast } from "react-toastify";
 
 export default function Posts({
   user,
   data,
+  totalPosts,
 }: {
   user: User;
   data: PostType[] | [];
+  totalPosts: number;
 }) {
   const supabase = createClient();
   const [posts, setPosts] = useState(data);
+  const [page, setPage] = useState(1);
+  const [noMoreData, setNoMoreData] = useState(false);
+  const limit = 4;
+  const { ref, inView } = useInView({ threshold: 0 });
+
+  // infinit scroll
+  useEffect(() => {
+    if (inView) {
+      fetchMoreFetch();
+    }
+  }, [inView]);
+
+  // fetchMoreFetch
+  const fetchMoreFetch = async () => {
+    let from = page * limit;
+    let to = from + limit;
+    if (from > totalPosts) {
+      setNoMoreData(true);
+      return false;
+    }
+    const { data: morePosts, error: error } = await supabase
+      .rpc("get_posts_with_likes", {
+        request_user_id: user.id,
+      })
+      .order("post_id", { ascending: false })
+      .range(from, to);
+    if (error) {
+      toast.error("faild to more fetch posts");
+      return;
+    }
+    setPage(page + 1);
+    const newMorePosts = morePosts as Array<PostType> | [];
+    if (newMorePosts && newMorePosts.length > 0) {
+      setPosts([...posts, ...newMorePosts]);
+    } else {
+      setNoMoreData(true);
+    }
+  };
 
   //   realtime changes
   useEffect(() => {
@@ -65,6 +108,14 @@ export default function Posts({
         posts.map((post: PostType) => (
           <PostCard key={post.post_id} post={post} user={user} />
         ))}
+      {!noMoreData && (
+        <div ref={ref} className="flex mt-2 justify-center">
+          <Loading />
+        </div>
+      )}
+      {noMoreData && (
+        <p className="text-center font-bold mt-2">No more posts ... !!</p>
+      )}
     </div>
   );
 }
